803b9bd6b7404c87e1a110063d47d2710bbe8c2b
Sang YOON <sangyoon@Sangui-MacBookPro.local>
2025-02-11 21:55:37 +0900

    current Time DB 시간 동기화 및 Repository Test 클래스 생성

---
+++ src/test/java/com/sh/point/application/service/point/TimeProviderTest.java
@@ -0,0 +1,19 @@
+package com.sh.point.application.service.point;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeProviderTest {
+	@Autowired
+	TimeProvider timeProvider;
+
+	@Test
+	public void currentTime() {
+		LocalDateTime currentTime = timeProvider.getCurrentTime();
+		System.out.println(currentTime);
+	}
+}
